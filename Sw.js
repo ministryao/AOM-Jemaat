4249b5a9e010bcbe56b7ad0d6e289f3f3c3522df
ministryao <ministry.ao@gmail.com>
2026-09-25 10:50:03 +0700

    Update Sw.js

--- Sw.js
+++ Sw.js
@@ -1,1 +1,57 @@
-
+const CACHE_NAME = "aom-jemaat-v1";
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+self.addEventListener(
+    "install",
+    event => {
+        event.waitUntil(
+            caches.open(CACHE_NAME)
+                .then(cache => {
+                    return cache.addAll(
+                        FILES_TO_CACHE
+                    );
+                })
+        );
+        self.skipWaiting();
+    }
+);
+self.addEventListener(
+    "activate",
+    event => {
+        event.waitUntil(
+            caches.keys()
+                .then(keys => {
+                    return Promise.all(
+                        keys
+                            .filter(
+                                key =>
+                                    key !== CACHE_NAME
+                            )
+                            .map(
+                                key =>
+                                    caches.delete(key)
+                            )
+                    );
+                })
+        );
+        self.clients.claim();
+    }
+);
+self.addEventListener(
+    "fetch",
+    event => {
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => {
+                    return caches.match(
+                        event.request
+                    );
+                })
+        );
+    }
+);
